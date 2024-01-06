e185fdc7583861158f69095bd92b95f3db3e8c21
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-06 00:15:00 -0800

    Create rio_minerals.tsx

---
+++ pages/rio_minerals.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+function RioMinerals() {
+    return (
+      <div id="screen-top">
+        <Navbar />
+        rio minerals
+    </div>
+  );
+}   
+
+export default RioMinerals;
