5d2615a13b14dff63c782d3642995eaa4ce4d8ee
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-06 00:14:56 -0800

    Create chat_bot.tsx

---
+++ pages/chat_bot.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+function ChatBot() {
+    return (
+      <div id="screen-top">
+        <Navbar />
+        chatbot
+    </div>
+  );
+}   
+
+export default ChatBot;
