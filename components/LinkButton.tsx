dcbbd8e423ec1d71a10936d03b30bf4ca3b79fc5
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-06 03:08:08 -0800

    Update LinkButton.tsx

--- components/LinkButton.tsx
+++ components/LinkButton.tsx
@@ -10,12 +10,12 @@ interface LinkButtonProps {
   const LinkButton: React.FC<LinkButtonProps> = ({ href, imgSrc, buttonText }) => {  
     return (
         <Link href={href} target="_blank" rel="noopener noreferrer">
-            <div className="flex items-center w-[115px] px-4 py-2 space-x-2 cursor-pointer rounded-[99px] bg-red">
+            <div className="flex items-center min-w-[115px] px-4 py-2 space-x-2 cursor-pointer rounded-[99px] bg-red">
                 <Image
                     src={`/${imgSrc}`}
                     alt={buttonText}
-                    width={18} // Adjust the size as needed
-                    height={18} // Adjust the size as needed
+                    width={18}
+                    height={18}
                 />
                 <span className="font-medium text-white">{buttonText}</span>
             </div>
