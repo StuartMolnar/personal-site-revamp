160e75ac7ba9b646f9298dc62143ffb633565605
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-06 00:15:02 -0800

    Create western_star.tsx

---
+++ pages/western_star.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+function WesternStar() {
+    return (
+      <div id="screen-top">
+        <Navbar />
+        western star
+    </div>
+  );
+}  
+
+export default WesternStar;
