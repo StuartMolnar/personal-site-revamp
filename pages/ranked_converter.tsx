913f4a56112fb4cb3bfb09e3458d069448c97ad5
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-06 00:14:58 -0800

    Create ranked_converter.tsx

---
+++ pages/ranked_converter.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+function RankedConverter() {
+    return (
+      <div id="screen-top">
+        <Navbar />
+        ranked converter
+    </div>
+  );
+}   
+
+export default RankedConverter;
