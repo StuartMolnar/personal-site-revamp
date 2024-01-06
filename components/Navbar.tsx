f381947c298f76e17d1a6e4c637e524602d34a98
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-06 04:48:48 -0800

    Adjust navbar responsiveness

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
     const router = useRouter();
 
     return (
-        <nav className="flex items-center justify-between px-32 pt-10 text-black bg-white text-header font-body">
+        <nav className="flex items-center justify-between px-10 pt-10 text-black bg-white md:px-32 text-header font-body">
             <div className="flex items-center">
                 <Link href="/">
                     <Image
@@ -19,10 +19,10 @@ function Navbar() {
             </div>
             <div className="flex items-center gap-4">
                 <Link href="/">
-                <p className={`hover:text-red ${router.pathname === '/' ? 'font-bold' : ''}`}>Home</p>
+                <p className={`hover:text-red ${router.pathname === '/' ? 'font-bold text-red' : ''}`}>Home</p>
                 </Link>
                 <Link href="/#projects" scroll={false}>
-                <p className={`hover:text-red ${router.pathname !== '/' ? 'font-bold' : ''}`}>Work & Projects</p>
+                <p className={`hover:text-red ${router.pathname !== '/' ? 'font-bold text-red' : ''}`}>Work & Projects</p>
                 </Link>
                 <a href="/resume.pdf" target="_blank" rel="noopener noreferrer">
                 <p className="hover:text-red">Résumé</p>
