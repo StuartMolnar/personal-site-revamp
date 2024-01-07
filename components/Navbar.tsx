b533de72658fa53224fc2bc88fe75bbeea0bea17
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-07 04:15:56 -0800

    Add mobile hamburger navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,12 +1,27 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import { useState, useEffect } from 'react';
 import { useRouter } from 'next/router';
+import { Spin as Hamburger } from 'hamburger-react'
 
 function Navbar() {
     const router = useRouter();
 
+    const [isOpen, setOpen] = useState(false);
+
+    useEffect(() => {
+        if (isOpen) {
+            document.body.classList.add('overflow-y-hidden');
+        } else {
+            document.body.classList.remove('overflow-y-hidden');
+        }
+
+        return () => document.body.classList.remove('overflow-y-hidden');
+    }, [isOpen]);
+
     return (
-        <nav className="flex items-center justify-between px-10 pt-10 text-black bg-white md:px-32 text-header font-body">
+        <>
+        <nav className="items-center justify-between hidden px-32 pt-10 text-black bg-white text-header font-body md:flex">
             <div className="flex items-center">
                 <Link href="/">
                     <Image
@@ -29,6 +44,28 @@ function Navbar() {
                 </a>
             </div>
         </nav>
+        <nav className="flex items-center justify-between pt-10 text-black bg-white md:hidden">
+
+            <Link href="/" className="z-30 pl-10">
+                <Image
+                    src={`Logo.svg`}
+                    alt={"Logo"}
+                    width={40}
+                    height={30}
+                />
+            </Link>
+            <div className={`z-30 pr-10 ${isOpen ? 'text-white' : 'text-black'}`}>
+                
+                <Hamburger toggled={isOpen} toggle={setOpen} direction="left" distance="sm" />
+            </div>
+
+            <div className={`z-20 flex flex-col gap-y-8 absolute items-center justify-center bg-black text-white text-header w-full h-full transition-all duration-300 ease-in-out ${isOpen ? 'top-0' : 'top-[-100%]'}`}>
+                <Link href="/" className={`${router.pathname === '/' ? 'font-bold' : ''}`}>Home</Link>
+                <Link href="/#projects" className={`${router.pathname !== '/' ? 'font-bold' : ''}`}>Work & Projects</Link>
+                <a href="/resume.pdf" target="_blank" rel="noopener noreferrer" className="">Résumé</a>
+            </div>
+        </nav>
+        </>
     );
   }
   
