9c565ad074575c5224b1909d4115a7e2eeb3abf5
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-14 20:16:06 -0800

    Update text sizes

--- pages/index.tsx
+++ pages/index.tsx
@@ -60,8 +60,8 @@ export default function Home() {
         {/* adjust grid-cols-# for different screen sizes if necessary */}
         <div id="projects" className="grid grid-cols-1 pt-20 space-y-4 md:gap-4 md:grid-cols-2 md:space-y-0">
           <Project bgColor="bg-gray" imgSrc="western-star-image.png" title="Western Star Resources" description="Front-End Development" href="western-star"/>
-          <Project bgColor="bg-black" imgSrc="league-logo.png" title="League of Legends Chat Assistant" description="Back-End Development" href="chat-bot" width_padding="p-12 lg:p-36"/>
           <Project bgColor="bg-black" imgSrc="leaguexvalorant-logo.png" title="League to Valorant Rank Converter" description="Full-Stack Development" href="ranked-converter" width_padding="p-10 lg:p-32"/>
+          <Project bgColor="bg-black" imgSrc="league-logo.png" title="League of Legends Chat Assistant" description="Back-End Development" href="chat-bot" width_padding="p-12 lg:p-36"/>
           <Project bgColor="bg-gray" imgSrc="rio-image.png" title="Rio Minerals Ltd." description="Front-End Development" href="western-star" width_padding="p-4"/>
         </div>
         
