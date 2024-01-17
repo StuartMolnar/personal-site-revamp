76277607f708f0f0adda2d868b95caa0bc9440d8
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-16 16:42:41 -0800

    Update Project.tsx

--- components/Project.tsx
+++ components/Project.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { useState } from 'react';
+import { MOBILE_BREAKPOINT } from "@/constants/constants";
 
 interface ProjectProps {
     bgColor: string;
@@ -9,14 +9,15 @@ interface ProjectProps {
     description: string;
     href: string;
     width_padding?: string;
+    onClick: () => void;
+    isActive: boolean;
   }
 
-function Project({ bgColor, imgSrc, title, description, href, width_padding }: ProjectProps) {
-    const [isLinkVisible, setIsLinkVisible] = useState(false);
+function Project({ bgColor, imgSrc, title, description, href, width_padding, isActive, onClick}: ProjectProps) {
 
     const handleMobileClick = () => {
-      if (window.innerWidth < 768) {
-          setIsLinkVisible(!isLinkVisible);
+      if (window.innerWidth < MOBILE_BREAKPOINT) {
+        onClick();
       }
   }
   
@@ -32,9 +33,9 @@ function Project({ bgColor, imgSrc, title, description, href, width_padding }: P
               />
           </div>
         </div>
-        <Link href={`/${href}`} className={`absolute inset-0 z-10 flex flex-col items-center text-center justify-center transition-opacity duration-300 bg-black bg-opacity-80 rounded-xl text-body ${isLinkVisible || 'md:group-hover:opacity-100'} ${isLinkVisible ? 'flex opacity-100' : 'hidden md:flex md:opacity-0'} text-white`}>
-          <h1 className="font-bold text-white text-normal md:text-title lg:text-title-lg max-w-[80%]">{title}</h1>
-          <p className="mt-2 text-body md:text-body-lg">{description}</p>
+        <Link href={`/${href}`} className={`absolute inset-0 z-10 flex flex-col items-center text-center justify-center transition-opacity duration-300 bg-black bg-opacity-80 rounded-xl text-body ${isActive || 'xl:group-hover:opacity-100'} ${isActive ? 'flex opacity-100' : 'hidden xl:flex xl:opacity-0'} text-white`}>
+          <h1 className="font-bold text-white text-title-sm 2sm:text-title-lg max-w-[80%]">{title}</h1>
+          <p className="mt-2 text-body-sm 2sm:text-body-lg">{description}</p>
           <button className="flex p-2.5 px-3 mt-4 space-x-2 text-body-sm rounded-3xl bg-red hover:brightness-125 hover:scale-105">
             <span>&#10132;</span><span>Read More</span></button>
         </Link>
