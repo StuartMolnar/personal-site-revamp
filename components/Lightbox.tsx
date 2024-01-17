b314e9c62e9dd9df53e62b9edb9cc2139d20b034
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-16 16:51:21 -0800

    Update Lightbox.tsx

--- components/Lightbox.tsx
+++ components/Lightbox.tsx
@@ -1,5 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import ReactDOM from 'react-dom';
+import {
+  disableBodyScroll,
+  enableBodyScroll,
+  clearAllBodyScrollLocks,
+} from 'body-scroll-lock-upgrade';
 
 interface LightboxProps {
   imgSrc: string;
@@ -8,49 +13,56 @@ interface LightboxProps {
 
 const Lightbox = ({ imgSrc, altText }: LightboxProps) => {
   const [isOpen, setIsOpen] = useState(false);
+  const lightboxRef = useRef<HTMLDivElement>(null); // Use useRef here
+
+  useEffect(() => {
+    // Now we access the current property of the ref
+    const targetElement = lightboxRef.current;
+    
+    if (isOpen && targetElement) {
+      disableBodyScroll(targetElement);
+    } else if (targetElement) {
+      enableBodyScroll(targetElement);
+    }
+    
+    // Clean up scroll locks when the component unmounts
+    return () => {
+      if (targetElement) {
+        enableBodyScroll(targetElement);
+      }
+      clearAllBodyScrollLocks();
+    };
+  }, [isOpen]);
 
   const toggleLightbox = () => {
     setIsOpen(!isOpen);
   };
 
-  
-  if (isOpen) {
-    document.body.style.overflow = 'hidden';
-  } else {
-    document.body.style.overflow = '';
-  }
-
   const lightboxContent = (
-    <div
-      className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80"
-      onClick={toggleLightbox}
-    >
+    <div ref={lightboxRef} className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-80" onClick={toggleLightbox} >
       <div className="max-h-full p-4 mx-6 overflow-auto bg-white rounded-lg xl:mx-32 md:mx-12">
         <img src={imgSrc} alt={altText} className="h-auto max-w-full" />
       </div>
-      <button
-        onClick={toggleLightbox}
-        className="absolute p-2 text-white bg-gray-700 rounded top-5 right-5 hover:bg-gray-600 focus:outline-none"
-      >
+      <button onClick={toggleLightbox} className="absolute p-2 text-white bg-gray-700 rounded top-5 right-5 hover:bg-gray-600 focus:outline-none">
         Close
       </button>
     </div>
   );
 
   return (
-      <div>
-        <img
-          src={imgSrc}
-          alt={altText}
-          onClick={toggleLightbox}
-          className="cursor-pointer"
-        />
-        {isOpen && ReactDOM.createPortal(
-          lightboxContent,
-          document.body
+    <div>
+      <img
+        src={imgSrc}
+        alt={altText}
+        onClick={toggleLightbox}
+        className="cursor-pointer"
+      />
+      {isOpen && ReactDOM.createPortal(
+        lightboxContent,
+        document.body
         )}
-      </div>
-    );
+    </div>
+  );
 };
-
+    
 export default Lightbox;
