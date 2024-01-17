d7ca1c6e8e9b0fc0b2a22dc6782bc76f29ce5845
StuartMolnar <stuartmolnar@hotmail.com>
2024-01-16 16:40:03 -0800

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {  
     return (
-        <footer className="flex justify-center py-8 bg-white text-body">
+        <footer className="flex justify-center px-6 py-8 bg-white text-body">
             <p className="">Message me at <a href="mailto:stuartmolnar@hotmail.com" className="font-semibold hover:text-red">stuartmolnar@hotmail.com</a></p>
         </footer>
     );
